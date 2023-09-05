142056e568f1751bfd88d1fff253d9956f8975e1
woolen-h <wobada@naver.com>
2023-09-05 17:15:36 +0900

    [Proj-01] CEREAL Magazine - Clone Coding - 0905 작업

--- src/js/script.js
+++ src/js/script.js
@@ -16,35 +16,45 @@ const films = document.querySelector("#films");
 const playlist = document.querySelector("#playlist");
 // shop 메뉴
 const shop = document.querySelector("#shop");
+// Image 캡션
+const imgCaption = document.querySelector("#imgCaption");
 
 travel.addEventListener("mouseover", function () {
     homeImage.style.backgroundImage = "url('/src/img/Patagonia12-1250x833.jpg')";
+    imgCaption.innerHTML="patagonia, chile";
 });
 
 art.addEventListener("mouseover", function () {
     homeImage.style.backgroundImage = "url('/src/img/Les-Palmiers1-1250x833.jpg')";
+    imgCaption.innerHTML="les palmiers, jean dunand";
 });
 
 design.addEventListener("mouseover", function () {
     homeImage.style.backgroundImage = "url('/src/img/KFM-landscape1_edit-1250x833.jpg')";
+    imgCaption.innerHTML="korean furniture museum, seoul";
 });
 
 style.addEventListener("mouseover", function () {
     homeImage.style.backgroundImage = "url('/src/img/Cartier-4-1250x833.jpg')";
+    imgCaption.innerHTML="iconic forms, cartier";
 });
 
 cityGuide.addEventListener("mouseover", function () {
     homeImage.style.backgroundImage = "url('/src/img/JGFrancis_OP2019_M1_DAY3C_CANYON_0021-1250x833.jpg')";
+    imgCaption.innerHTML="los angeles";
 });
 
 films.addEventListener("mouseover", function () {
     homeImage.style.backgroundImage = "url('/src/img/00009951008-2-1250x833.jpg')";
+    imgCaption.innerHTML="films";
 });
 
 playlist.addEventListener("mouseover", function () {
     homeImage.style.backgroundImage = "url('/src/img/Summer-playlist-grid-1250x833.jpg')";
+    imgCaption.innerHTML="playlist";
 });
 
 shop.addEventListener("mouseover", function () {
     homeImage.style.backgroundImage = "url('/src/img/Home-1250x833.jpg')";
+    imgCaption.innerHTML="";
 });
